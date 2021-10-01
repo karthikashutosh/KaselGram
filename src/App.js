65953c6e41510fc6424525627891b377cb5c2446
karthik selvam <41238125+karthikashutosh@users.noreply.github.com>
2021-10-01 10:27:24 +0530

    small update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Title from './comps/Title';
+import Title from './Components/Title';
 
 function App() {
   return (
---
+++ src/Components/Title.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>kasel-Gram</h1>
+      <hr />
+      <h2>Your Pictures</h2>
+      <p>Upload your picture to Here</p>
+    </div>
+  );
+};
+
+export default Title;
