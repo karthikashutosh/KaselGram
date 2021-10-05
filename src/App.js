f4741925c4611dc9ceea0a1b64853de9abd0ef66
karthik selvam <41238125+karthikashutosh@users.noreply.github.com>
2021-10-05 20:41:53 +0530

    sign in added

--- src/App.js
+++ src/App.js
@@ -4,8 +4,17 @@ import Explore from "./Pages/Explore";
 import Home from "./Pages/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Title from "./Components/Title";
+import Login from "./Components/Login";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./Firebase/config";
 
 function App() {
+  const [user] = useAuthState(auth);
+
+  if (!user) {
+    return <Login />;
+  }
+
   return (
     <div className="App">
       <Router>
---
+++ src/Components/Login.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Login.css";
+import { auth, provider } from "../Firebase/config";
+import { Button } from "@material-ui/core";
+
+export default function Login() {
+  function login() {
+    auth.signInWithRedirect(provider);
+  }
+  return (
+    <div className="app">
+      <div className="login">
+        <div className="login__container">
+          <img src="./login-logo.png" alt="logo" />
+          <div className="login__text">
+            <h1>Sign in KaselGram</h1>
+          </div>
+          <Button className="button" onClick={login}>
+            Sign in With Google
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
