660d6ddbbed427edb4b1408b9e71d34de7539c2c
karthik selvam <41238125+karthikashutosh@users.noreply.github.com>
2021-10-05 12:57:13 +0530

    done

---
+++ src/Components/Darkmode.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Darkmode() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Darkmode from "../Components/Darkmode";
 
 const Title = () => {
   return (
     <div className="title">
       <h1>kasel-Gram</h1>
-      <div className="nav-bar" >
-        <Link to="/"style={{textDecoration:"none"}}><h1>Home</h1></Link>
-        {/* <Darkmode /> */}
-        <h1>Dark mode</h1>
-        <Link to="/explore" style={{textDecoration:"none"}}><h1>Explore</h1></Link>
+      <div className="nav-bar">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <h1>Home</h1>
+        </Link>
+        <Darkmode />
+        <Link to="/explore" style={{ textDecoration: "none" }}>
+          <h1>Explore</h1>
+        </Link>
       </div>
       <hr />
     </div>
